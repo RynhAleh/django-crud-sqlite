2316811a4c93facfe8d04300f1db9894e754aabd
olegring <workinplus@yandex.ru>
2024-10-12 00:19:35 +0300

    deleting sessions

--- delete_all_sessions.sh
+++ delete_all_sessions.sh
@@ -1,2 +1,10 @@
-if [[ -z "$VIRTUAL_ENV" ]]; then source .venv/bin/activate; fi
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "The virtual environment is not activated."
+    echo "Activating the virtual environment..."
+    source .venv/bin/activate
+else
+    echo "The virtual environment is already activated: $VIRTUAL_ENV"
+fi
+echo "Deleting all sessions..."
 echo "from django.contrib.sessions.models import Session; Session.objects.all().delete()" | python3 manage.py shell
+echo "OK."
